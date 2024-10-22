76df0f0c43a39d223c413b5a3eb789675926179a
Ali Ucar <forbns12@gmail.com>
2024-10-22 03:56:46 +0300

    Who fixed

--- Pong.cpp
+++ Pong.cpp
@@ -6,6 +6,9 @@ Pong::Pong()
 
 void Pong::execute(int client_fd)
 {
+	// std::cout << _users->getNickName() << "mustmustumsutmustmustu"<<std::endl;
+	// std::string message = ":" + _users->getNickName()+ "!"+ _users->getName() + "@" + _server->getHost()+ " PONG :" + _users->getNickName();
+	// _server->sendMessage(client_fd, message);   
 }
 
 std::string Pong::getName() const
--- Who.cpp
+++ Who.cpp
@@ -6,6 +6,7 @@ Who::Who()
 
 void Who::execute(int client_fd)
 {
+	bool channelExists = 0;
 	//server get users ile kullanıcı alıp tüm kullanıcılar için kanal ismi eşleşiyorsa eşleşen kullanıcıları yazdır.
 	if (_args.size() == 2)
 	{
@@ -17,12 +18,11 @@ void Who::execute(int client_fd)
 				std::vector<std::string> userChannels = (*us_it)->getChannelName();
 				for (std::vector<std::string>::iterator it = userChannels.begin(); it != userChannels.end(); ++it)
 				{
-					if((*it).data()[0] != '\0' && (*it).data() && (*it).data() == _args[1])
-						_server->sendMessage(client_fd, "Nick: " + (*us_it)->getNickName() + "\n");
-					else if((it+1) == userChannels.end())
+					std::cout << (*it).data() << _args[1] << std::endl;
+					if((*it).data()[0] != '\0' && (*it).data() && (*it) == _args[1])
 					{
-						_server->sendError(client_fd, "Channel not found\n");
-						return;
+						_server->sendMessage(client_fd, "Nick: " + (*us_it)->getNickName() + "\n");
+						channelExists = 1;
 					}
 				}
 			}
@@ -34,6 +34,11 @@ void Who::execute(int client_fd)
 			else if((us_it+1) == users.end())
 			{
 				_server->sendError(client_fd, "User not found\n");
+				return;
+			}
+			if(!channelExists && us_it+1 == users.end())
+			{
+				_server->sendError(client_fd, "Channel not found\n");
 				return;
 			}
 		}
